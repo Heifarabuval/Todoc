19e22acd57a5e350dd0fe1b9c1df48b428dfe1ff
Heifara <heifara.buval@gmail.com>
2020-06-02 13:14:36 +0200

    Final Project

--- app/src/androidTest/java/com/cleanupH/todo/MainActivityInstrumentedTest.java
+++ app/src/androidTest/java/com/cleanupH/todo/MainActivityInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.cleanup.todoc;
+package com.cleanupH.todo;
 
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -6,7 +6,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
 
-import com.cleanup.todoc.ui.MainActivity;
+
+import com.cleanup.todocH.R;
+import com.cleanup.todocH.ui.MainActivity;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -18,7 +20,7 @@ import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static com.cleanup.todoc.TestUtils.withRecyclerView;
+import static com.cleanupH.todo.TestUtils.withRecyclerView;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
@@ -31,7 +33,7 @@ import static org.junit.Assert.assertThat;
 @RunWith(AndroidJUnit4.class)
 public class MainActivityInstrumentedTest {
     @Rule
-    public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class);
+    public final ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
     public void addAndRemoveTask() {
